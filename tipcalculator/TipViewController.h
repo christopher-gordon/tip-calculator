f360f63fba5b85ba10a3c3a2dd04e303b828414a
Chris Gordon <ChristopherScottGordon@gmail.com>
2015-09-27 14:44:05 -0500

    TipViewController setup

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Chris Gordon on 9/27/15.
+//  Copyright (c) 2015 Chris Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.m
@@ -0,0 +1,46 @@
+//
+//  TipViewController.m
+//  tipcalculator
+//
+//  Created by Chris Gordon on 9/27/15.
+//  Copyright (c) 2015 Chris Gordon. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
